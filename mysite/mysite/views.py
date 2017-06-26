262fedbc19beef987978830d64bbe6d281c10a7a
Rahul Madhavan <rahul.maddy@gmail.com>
2017-06-27 01:37:17 +0530

    Started so late!

---
+++ mysite/mysite/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse,Http404
+import datetime
+
+def index(request):
+    return HttpResponse("Hello World!")
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html= "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+def hours_ahead(request,hours_ahead):
+    try:
+        offset = int(hours_ahead)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be  %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
