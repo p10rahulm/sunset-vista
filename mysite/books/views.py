8de93c2f57715a96b588b6ace1da0120dcc32ff5
Rahul Madhavan <rahul.maddy@gmail.com>
2017-06-28 03:08:48 +0530

    still on forms

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from books.models import Book
 # Create your views here.
 
 def search_form(request):
     return render(request, 'books/search_form.html')
 
 def search(request):
-    if 'q' in request.GET:
-        message = "You searched for : %r" % request.GET['q']
+    if 'q' in request.GET and request.GET['q']:
+        q = request.GET['q']
+        #message = "You searched for : %r" % request.GET['q']
+        books = Book.objects.filter(title__icontains=q)
+        return render(request,"books/search_results.html",{'books':books,'query':q})
     else:
         message = "You submitted an empty form"
-    return  HttpResponse(message)
+        return  HttpResponse(message)
+
